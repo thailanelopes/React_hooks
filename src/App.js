3ed2ae3e902d7cad50d0dfc7643bdb4bff28df2e
thailane.dutra <thailane111122@gmail.com>
2023-04-09 17:01:23 -0300

    construindo o menu

--- src/App.js
+++ src/App.js
@@ -6,6 +6,15 @@ import Usuarios from './components/Usuarios/Usuarios'
 function App() {
   return (
     <div className="App">
+      <header>
+        <nav>
+          <ul>
+            <li><a href='/'>Inicio</a></li>
+            <li><a href='/usuarios'>Usuarios Cadastrados</a></li>
+            <li><a href='/adicionar'>Adicionar Usuarios</a></li>
+          </ul>
+        </nav>
+      </header>
       <main>
         <Usuarios />
       </main>
